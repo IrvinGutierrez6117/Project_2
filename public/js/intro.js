61a45a87e070cf7506d614a8e41527c24d111b80
notmsc <mscaro22@gmail.com>
2019-03-23 22:37:24 -0700

    update some of the front end ui modals,materialize..etc

---
+++ public/js/intro.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.modal').modal();
+//modal for new user
+    //create a post method to the database 
+
+//modal for existing user
+    //create a get request from existing database
+    //and maybe make it somehow verify 
+
+});
