7973508285626c90deda5a719fe2653dfdfaaf08
notmsc <mscaro22@gmail.com>
2019-03-25 14:49:55 -0700

    removed my form tags to keep one form for all modals and cleaned up spacing

--- public/js/intro.js
+++ public/js/intro.js
@@ -1,39 +1,41 @@
-var newUser = true;
-// change to false for Login/Existing user
-
 $(document).ready(function(){
+    var newUser = true;
+    // change to false for Login/Existing user
+    //initialize modal materialize
     $('.modal').modal();
-});
-
-// when user clicks any one of the emojis, it gets stored in a variable
-var emoji;
-// on click function for emoji selected
-$(".emoji-choice").on("click", function(e) {
-    // shows what emoji is clicked. 
-    // needs to be stored in DB 
-    emoji = e.currentTarget.id;
-    console.log(emoji);
-    // Materialize text area
+    //initialize textarea materialize
     $('input#input_text, textarea#textarea2').characterCounter();
 
+    // when user clicks any one of the emojis, it gets stored in a variable
+    var emoji;
+    // on click function for emoji selected
+    $(".emoji-choice").on("click", function(e) {
+        // shows what emoji is clicked. 
+        // needs to be stored in DB 
+        emoji = e.currentTarget.id;
+        console.log(emoji);
+        // Materialize text area
+
+    });
+    // Update/put if user logs in
+    // $("#login-existing-button").on("click", function(e){
+    //     newUser = false;
+    //     // Backend question, what is the table called for the db to store info
+    //     db.tableName.findOne().then(function(){
+    //         //.findOne will find a table entry you are searching
+
+    //     })
+    // });
+    //Post info into the server
+    // if user is new, create them and use findOne to get them from the database
+
+    //create a post method to the database for both users
+    //create a get request from existing database
+    // function updateUsersDB() {
+    //     if (newUser === true) {
+            
+    //     }
+
+    // }
 });
-// Update/put if user logs in
-// $("#login-existing-button").on("click", function(e){
-//     newUser = false;
-//     // Backend question, what is the table called for the db to store info
-//     db.tableName.findOne().then(function(){
-//         //.findOne will find a table entry you are searching
 
-//     })
-// });
-//Post info into the server
-// if user is new, create them and use findOne to get them from the database
-
-//create a post method to the database for both users
-//create a get request from existing database
-// function updateUsersDB() {
-//     if (newUser === true) {
-        
-//     }
-
-// }
