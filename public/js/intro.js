3c473cad4a49efc7114c03e112f92cc4980fe3fa
notmsc <mscaro22@gmail.com>
2019-03-27 17:26:03 -0700

    Added functions and onclick for user and existing user ids to start initial code

--- public/js/intro.js
+++ public/js/intro.js
@@ -3,6 +3,19 @@ $(document).ready(function(){
     $('.modal').modal();
     //initialize textarea materialize
     $('input#input_text, textarea#textarea2').characterCounter();
+    //===================== materialize =========================
+
+    var newOrExistUser = null;
+    var allPostInfo = null;
+
+    var newUser = false;
+    var existingUser = false;
+
+    var newUsername = $("#intro-new-name").val().trim();
+    var existingUsername = $("#intro-existing-name").val().trim();
+    
+    var newPassword = $("#intro-new-password").val().trim();
+    var existingPassword = $("#intro-existing-password").val().trim();
 
     //This will get the value of each input for the form
     function isRadioAnswer (x) {
@@ -16,7 +29,30 @@ $(document).ready(function(){
         }
     }
 
+    //This function is used to activate the clicks for when a user selects to be a new or an existing user
+    function isUserClick (x) {
+        if (newUser === true) {
+            //post method for 
 
+            return newOrExistUser = //something; && newUsername && newPassword
+        } else if (existingUser === true) {
+            //post method for existing user
+
+            return newOrExistUser = //something; && existingUsername && existingPassword
+        }
+    }
+    // When user clicks Create Account button        
+    $("#intro-new-user-modal").on("click", function() {
+        newUser = true;
+        existingUser = false;
+        isUserClick(newUser);
+    });
+    // When user clicks Login button
+    $("#intro-existing-user-modal").on("click", function() {
+        existingUser = true;
+        newUser = false;
+        isUserClick(existingUser);
+    });
 
     $("#intro-submit-entry").on("click", function() {
         
@@ -30,6 +66,9 @@ $(document).ready(function(){
         console.log(newPost);
 
         //post method to database what alex said
+        $.post("/api/entries" + newOrExistUser.id,
+         newPost).then();
+        
     });
 
 });
